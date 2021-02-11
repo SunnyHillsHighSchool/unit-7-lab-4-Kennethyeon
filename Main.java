aefe07db17afa8b0baefc26538b61d1f0747812c
Kennethyeon1 <600020446@fjuhsd.org>
2021-02-11 18:31:51 +0000

    Completed unit 7 lab 4

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 //(c) A+ Computer Science
 //www.apluscompsci.com
-//Name -
+//Name - Kenneth Yeon
 
 public class Main
 {
@@ -8,7 +8,21 @@ public class Main
 	{
 		
             int[] nums = {7,28,496,1111,199,201,17};
+            for( int num : nums )
+            {
+              Number one = new Number( num );
+              if ( one.isOdd() )
+	            System.out.print( one + " is odd.\n" );
+              else 
+	            System.out.print( one + " is not odd.\n" );
+	
+              if ( one.isPerfect() )
+	            System.out.print( one + " is perfect.\n" );
+              else 
+	            System.out.print( one + " is not perfect.\n" );
+            }
             //see readme file for additional code
+            
            
 	}
 }
--- Number.java
+++ Number.java
@@ -1,6 +1,6 @@
 //(c) A+ Computer Science
 //www.apluscompsci.com
-//Name -
+//Name - Kenneth Yeon
 
 
 //write the Number class
@@ -9,20 +9,77 @@
 //to guide you
 
 //header for Number class
-
+public class Number 
+{
 //private instance variable (must be an Integer)
+private Integer num;
 
 //constructor
+public Number()
+{
+  num = 0;
+}
 
-
+public Number(Integer n)
+{
+  num = n;
+}
 //getter method
-
+public Integer getNum()
+{
+  return num;
+}
 
 //setter method
+public void setNum(int n)
+{
+  num = n; 
+}
 
 //isOdd method
-
+public boolean isOdd()
+{
+  //if the remainder of dividing the number by 2 is 1 return true
+  if (num % 2 == 1)
+  {
+    return true;
+  }
+  //else return false
+  else
+  {
+    return false;
+  }
+}
 //isPerfect method
-
+public boolean isPerfect()
+{
+  //create an int variable called sum equal to zero
+  int sum = 0;
+  //create a for loop that starts from 1 and goes up to the number - 1
+  for (int i = 1; i <= num - 1; i++)
+  {
+    //if the remainder of the number divided by the value of the loop is zero add value of the loop to sum
+    if (num % i == 0)
+    {
+      sum = sum + i;
+    }
+    //end for loop
+  }
+  //if sum is equal to the number return true
+  if (sum == num)
+  {
+    return true;
+  }
+  //else return false 
+  else
+  {
+    return false;
+  }
+}
 
 //toString
+  public String toString()
+  {
+    return String.valueOf(num);
+  }
+}
